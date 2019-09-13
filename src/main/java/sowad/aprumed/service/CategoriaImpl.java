219b4495504642e877bbb4f861977398e0a37960
Miguel <duke_miguel99@hotmail.com>
2019-09-13 11:59:44 -0500

    CategoriaImpl 1

--- src/main/java/sowad/aprumed/service/CategoriaImpl.java
+++ src/main/java/sowad/aprumed/service/CategoriaImpl.java
@@ -1,5 +1,35 @@
 package sowad.aprumed.service;
 
-public class CategoriaImpl {
+import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import sowad.aprumed.dao.CategoriaDao;
+import sowad.aprumed.model.Categoria;
+
+public class CategoriaImpl implements CategoriaDao{
+	private DataSource dataSource;
+	private JdbcTemplate jdbcTemplate;
+	
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource=dataSource;
+	}
+	public int insertarCategoria(Categoria categoria) {
+		String query="insertarCategoria(?,?)";
+		jdbcTemplate = new JdbcTemplate(dataSource);
+		Object[] inputs = new Object[]
+				{categoria.getNombreCategoria(),
+				 categoria.getEstado()};
+		return jdbcTemplate.update(query, inputs);
+	}
+	public Categoria buscarCategoria(String nombreCategoria) {
+		String query="buscarCategoria(?)";
+		jdbcTemplate = new JdbcTemplate(dataSource);
+		return jdbcTemplate.update(query, nombreCategoria);
+	}
+	public int editarCategoria(Categoria categoria);
+	public int eliminarCategoria(Categoria categoria);
+	public List<Categoria> mostrarCategoria(Categoria categoria);
 }
