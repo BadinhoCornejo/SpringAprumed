19feb02305232ccf8b8223a9879519739b000354
Miguel <duke_miguel99@hotmail.com>
2019-09-13 11:30:13 -0500

    CategoriaDao agregado

--- src/main/java/sowad/aprumed/dao/CategoriaDao.java
+++ src/main/java/sowad/aprumed/dao/CategoriaDao.java
@@ -1,5 +1,13 @@
 package sowad.aprumed.dao;
 
-public class CategoriaDao {
+import java.util.List;
 
+import sowad.aprumed.model.Categoria;
+
+public interface CategoriaDao {
+	public int insertarCategoria(Categoria categoriaDao);
+	public Categoria buscarCategoria(Categoria categoriaDao);
+	public int editarCategoria(Categoria categoriaDao);
+	public int eliminarCategoria(Categoria categoriaDao);
+	public List<Categoria> mostrarCategoria(Categoria categoriaDao);
 }
